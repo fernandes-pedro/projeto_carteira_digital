bf037fbad3e11379c7dbe72617469805c8f36c66
ItaloVasconcelo05 <italomiguel9705@gmail.com>
2025-12-04 01:12:36 -0300

    refactor: Ajuste das estruturas da class

--- api/models/carteira_models.py
+++ api/models/carteira_models.py
@@ -4,17 +4,20 @@ from pydantic import BaseModel
 from decimal import Decimal
 
 
-class Carteira(BaseModel):
+class CarteiraCriada(BaseModel):
     endereco_carteira: str
+    chave_privada: str
     data_criacao: datetime
     status: Literal["ATIVA","BLOQUEADA"]
-
-class CarteiraCriada(Carteira):
-    chave_privada: str
-
-class Saldo(BaseModel):
-    id_moeda: int
+    
+class CarteiraBase(BaseModel):
+    endereco_carteira: str
+    data_criacao: datetime
+    status_ativo: str
+class SaldoItem(BaseModel):
     codigo_moeda: str
-    nome_moeda: str
     saldo: Decimal
-    data_atualizacao: datetime
+
+class CarteiraSaldoResponse(BaseModel):
+    endereco_carteira: str
+    saldos: list[SaldoItem]
